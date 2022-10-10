4f92c38e88f4b37d4d989557f386f530e621f1cc
K_DOUK03 <caley329@naver.com>
2022-10-11 02:02:04 +0900

    study

--- Game/shooting_game.py
+++ Game/shooting_game.py
@@ -13,14 +13,54 @@
 # [게임 이미지]
 # 1. 배경 : 640 * 480
 # 2. 무대 : 640 * 50
-# 3. 캐릭터 : 60 * 33
+# 3. 캐릭터 : 33 * 60
 # 4. 무기 : 20 * 430
 # 5. 공 : 160 * 160, 80 * 80, 40 * 40, 20 * 20
 
 import pygame
+import os
 
 pygame.init()
 
 screen_width = 640
 screen_height = 480
 screen = pygame.display.set_mode((screen_width, screen_height))
+
+pygame.display.set_caption("Pang")
+
+clock = pygame.time.Clock()
+
+current_path = os.path.dirname(__file__) # 현재 파일 위치 반환
+image_path = os.path.join(current_path, "game image")
+
+# background = pygame.image.load(os.path.join(image_path, "background.png"))
+background = pygame.image.load("/Users/kd_mb/Desktop/code/Python/Game/game image/background.png")
+
+# stage = pygame.image.load(os.path.join(image_path, "stage.png"))
+stage = pygame.image.load("/Users/kd_mb/Desktop/code/Python/Game/game image/stage.png")
+stage_size = stage.get_rect().size
+stage_height = stage_size[1] # stage 위에 character를 세워놓기 위해 계산을 통한 변수생성
+
+# character = pygame.image.load(os.path.join(image_path), "character.png")
+character = pygame.image.load("/Users/kd_mb/Desktop/code/Python/Game/game image/character.png")
+character_size = character.get_rect().size  
+character_width = character_size[0]
+character_height = character_size[1]
+character_x_pos = (screen_width / 2) + (character_width / 2)
+character_y_pos = screen_height - stage_height - character_height
+
+running = True
+while running:
+    dt = clock.tick(30)
+    
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    
+    screen.blit(stage, (0, screen_height - stage_height))
+    screen.blit(background, (0, 0))
+    screen.blit(character, (character_x_pos, character_y_pos))
+    
+    pygame.display.update()
+    
+pygame.quit()
