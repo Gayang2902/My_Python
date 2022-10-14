0818f811f8ea762299cd4bba59e92639b60ffcbd
K_DOUK03 <caley329@naver.com>
2022-10-15 02:47:48 +0900

    study at del?

--- Pygame/shooting_game.py
+++ Pygame/shooting_game.py
@@ -47,7 +47,7 @@ character_size = character.get_rect().size
 character_width = character_size[0]
 character_height = character_size[1]
 character_x_pos = (screen_width / 2) - (character_width / 2)
-character_y_pos = screenn_height - stage_height - character_height
+character_y_pos = screen_height - stage_height - character_height
 
 character_to_x = 0
 
@@ -62,24 +62,28 @@ weapons = []
 weapon_speed = 10
 
 ball_images = [
-    pygame.image.load(os.path.join(image_path, "ballon1.png")),
-    pygame.image.load(os.path.join(image_path, "ballon2.png")),
-    pygame.image.load(os.path.join(image_path, "ballon3.png")),
-    pygame.image.load(os.path.join(image_path, "ballon4.png"))]
+    pygame.image.load(os.path.join(image_path, "balloon1.png")),
+    pygame.image.load(os.path.join(image_path, "balloon2.png")),
+    pygame.image.load(os.path.join(image_path, "balloon3.png")),
+    pygame.image.load(os.path.join(image_path, "balloon4.png"))]
 
 # 공 크기에 따른 최초 스피드
 ball_speed_y = [-18, -15, -12, -9]
 
 balls = []
 
-
-# 공은 여러개가 존재하고 각각 좌우상하 움직임이 다르기 때문에 dictionary를 이용해 세분화 시켜준다. 
-balls.apeend({
+# 공은 여러개가 존재하고 각각 좌우상하 움직임이 다르기 때문에 dictionary를 이용해 세분화 시켜준다. 아래는 최초 발생하는 큰 공
+balls.append({
     "pos_x" : 50,
     "pos_y" : 50,
     "image_index" : 0,
-    
-})
+    "to_x" : 3, 
+    "to_y" : -6,
+    "init_speed_y" : ball_speed_y[0]})
+
+# 사라질 무기와 공(무기와 공이 닿으면 무기는 사라지고, 공은 사라진뒤 해당 자리에 작은 공이 생김)
+weapon_to_remove = -1
+weapon_to_remove = -1
 
 running = True
 while running:
@@ -117,10 +121,73 @@ while running:
         
     # 천장에 닿지 않은 weapon들만 리스트에 재저장핢으로써 천장에 닿은 weapon은 리스트에 들어가지 않기에 더는 그려지지 않는다.
     weapons = [[w[0], w[1]] for w in weapons if w[1] > 0]
+    
+    # 공 위치 정의
+    for ball_index, ball_value in enumerate(balls):
+        ball_pos_x = ball_value["pos_x"]
+        ball_pos_y = ball_value["pos_y"]
+        ball_image_index = ball_value["image_index"]
+        
+        ball_size = ball_images[ball_image_index].get_rect().size
+        ball_width = ball_size[0]
+        ball_height = ball_size[1]
+        
+        if ball_pos_x <= 0 or ball_pos_x >= screen_width - ball_width:
+            ball_value["to_x"] *= -1
+            
+        if ball_pos_y >= screen_height - stage_height - ball_height:
+            ball_value["to_y"] = ball_value["init_speed_y"]
+        else:
+            ball_value["to_y"] += 0.5
+            
+        ball_value["pos_x"] += ball_value["to_x"]
+        ball_value["pos_y"] += ball_value["to_y"]
+
+    # 각 개체들의 rect 정보 업데이트(위치가 먼저 정의되어야 하기 때문에 위치 정의 파트 아래에 작성)
+    character_rect = character.get_rect()
+    character_rect.left = character_x_pos
+    character_rect.top = character_y_pos
+    
+    for ball_index, ball_value in enumerate(balls):
+        ball_pos_x = ball_value["pos_x"]
+        ball_pos_y = ball_value["pos_y"]
+        ball_image_index = ball_value["image_index"]
+        
+        ball_rect = ball_images[ball_image_index].get_rect()
+        ball_rect.left = ball_pos_x
+        ball_rect.top = ball_pos_y
+        
+        if character_rect.colliderect(ball_rect):
+            running = False
+            break
+        
+        for weapon_index, weapon_value in enumerate(weapons):
+            weapon_pos_x = weapon_value[0]
+            weapon_pos_y = weapon_value[1]
+            
+            weapon_rect = weapon.get_rect
+            weapon_rect.left = weapon_pos_x
+            weapon_rect.top = weapon_pos_y
+            
+            if weapon_rect.colliderect(ball_rect):
+                weapon_to_remove = weapon_index
+                ball_to_remove = ball_index
+                break
+            
+    # 충돌된 공 or 무기 없애기 
+    if ball_to_remove > -1:
+        del balls([ball_to_remove]) # 10/15 2:47 am
             
     screen.blit(background, (0, 0))
     for weapon_x_pos, weapon_y_pos in weapons:
         screen.blit(weapon, (weapon_x_pos, weapon_y_pos))
+    
+    for index, value in enumerate(balls):
+        ball_pos_x = value["pos_x"]
+        ball_pos_y = value["pos_y"]
+        ball_image_index = value["image_index"]
+        screen.blit(ball_images[ball_image_index], (ball_pos_x, ball_pos_y))
+    
     screen.blit(character, (character_x_pos, character_y_pos))        
     screen.blit(stage, (0, screen_height - stage_height))
     
